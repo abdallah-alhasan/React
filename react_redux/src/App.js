dce3e37ec501d4ab18043939efe738aeb646b437
abdallah-alhasan <eng.abdallahalhasan@gmail.com>
2022-07-22 02:25:48 +0300

    Done

---
+++ react_redux/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+import {increment, decrement} from './Actions'
+
+function App() {
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+  return (
+    <div className="App">
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_redux/src/Reducers/index.js
@@ -0,0 +1,10 @@
+import CounterReducer from "./Counter";
+import { combineReducers } from "redux";
+
+const allReducers = combineReducers(
+    {
+        counter: CounterReducer
+    }
+)
+
+export default allReducers
